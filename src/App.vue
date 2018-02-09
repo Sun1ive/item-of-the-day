14df9ce2eace9cd61d3e76ad8defc007e75bec4e
Sun1ive <sunliveua@gmail.com>
2018-02-09 13:41:49 +0200

    upd

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld,
   },
@@ -17,12 +17,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
+import './style/main.styl';
 
 Vue.config.productionTip = false;
 
